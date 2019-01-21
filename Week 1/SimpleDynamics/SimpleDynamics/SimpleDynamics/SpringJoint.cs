1d631993037dd29449c0ef347dca90504369875f
J.Pritchard U1661665 <U1661665@unimail.hud.ac.uk>
2019-01-21 17:00:36 +0000

    21.01.19 File organization, Exercise 1 WIP

--- Week 1/SimpleDynamics/SimpleDynamics/SimpleDynamics/SpringJoint.cs
+++ Week 1/SimpleDynamics/SimpleDynamics/SimpleDynamics/SpringJoint.cs
@@ -33,6 +33,32 @@ namespace SimpleDynamics
             //Apply the spring force to the two bodies joined by the spring
             //Note: The spring force has a direction which is given by the vector between BodyB and BodyA
             // The magnitude of the spring force is caculated using the Hooke's law
+
+            //Only apply calculations if the length is greater than the rest length.
+            if (length > RestLength)
+            {
+
+                length -= RestLength;
+                
+
+                //Compute the spring force based on Hooke's law.
+                // F = -kx
+                float hookesForce = -(Stiffness * length);
+
+                //Create a vector of hookesForce.
+                Vector2D hookesForceVec = new Vector2D(hookesForce, hookesForce);
+
+                //normalise the vector between the two masses.
+                s_vec.X = s_vec.X / s_vec.Length();
+                s_vec.Y = s_vec.Y / s_vec.Length();
+
+                //Create a vector for force proportional to the hookesForce to be applied.
+                hookesForceVec.X *= s_vec.X;
+                hookesForceVec.Y *= s_vec.Y;
+
+                //Apply the force to body A.
+                BodyB.Force += hookesForceVec;
+            }
         }
     }
 }
